7812c29496aa01c893358f6d004ac4062d8d999e
Pawel <szalewskipawel@gmail.com>
2019-01-25 14:20:59 +0100

    BEFORE adding NativeModules getControllers

--- BottomRightPanel.js
+++ BottomRightPanel.js
@@ -14,13 +14,27 @@ export default class BottomRightPanel extends React.Component {
         };
 
         //this.tick = this.tick.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(e) {
+        const event = e.nativeEvent;
+        const inputEvent = event.inputEvent;
+
+        // if(inputEvent.action == 'up') {
+        //     let letter = String.fromCharCode(inputEvent.button);
+        //     console.log(letter);
+        //     console.log('======');
+        //     this.setState({ input1: this.state.input1 + letter});
+        // }
+        this.setState({ input1: this.state.input1 + 'X'});
     }
 
     render() {
         let displayString = this.state.input1;
 
         return (
-            <View style={styles.panel}>
+            <View style={styles.panel} onInput={this.handleInput} >
                 <View style={styles.greetingBox}>
                     <Text style={styles.greeting}>
                         BottomRightPanel { displayString }
@@ -43,6 +57,8 @@ const styles = StyleSheet.create({
             // { rotateX: 90 },
             { translateX: 1100 },
             { translateY: -650 }
+            // { translateX: 0 },
+            // { translateY: 0 }
         ],
         borderColor: 'blue',
         borderWidth: 2,
